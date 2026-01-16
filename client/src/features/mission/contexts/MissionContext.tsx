40874087748c491f06dfbfdfe496323fb8c68bd0
Rahul raikwar <rr200636@gmail.com>
2026-01-17 01:16:13 +0530

    Remove console log for mission states
    Removed console log for active mission states.

--- client/src/features/mission/contexts/MissionContext.tsx
+++ client/src/features/mission/contexts/MissionContext.tsx
@@ -99,7 +99,6 @@ export const MissionContextProvider = ({
     };
     hydrate();
   }, []);
-  console.log("acive mission", missionStates);
   useEffect(() => {
     if (!isInitialized || missionStates.missionStatus === "idle") return;
     StorageService.saveMission(missionStates);
